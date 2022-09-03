4755c4efccf965ae3e16b42fc2bcf102be4feb69
Adesipe Adeola <princeadeola234@gmail.com>
2022-09-03 12:08:22 -0700

    committed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useSelector, useDispatch} from 'react-redux';
 import {increment, decrement} from './actions';
 
 function App() {
-  const counter = useSelector(state => state.counter);
-  const isLogged = useSelector(state => state.isLogged);
-  const dispatch = useDispatch();
-
+  // const counter = useSelector(state => state.counter);
+  // const isLogged = useSelector(state => state.isLogged);
+  // const dispatch = useDispatch();
+ 
   return (
     <div className="App">
       <h1>Hello Redux</h1>
